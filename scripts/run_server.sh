1564d45870c172d81c2eb81f27c64b4fade653a5
Ubuntu <ubuntu@ip-172-31-45-115.ec2.internal>
2024-05-20 09:27:23 +0000

    Server is working. Replica is ready.

--- scripts/run_server.sh
+++ scripts/run_server.sh
@@ -1,14 +1,13 @@
-if [ "$#" -ne 7 ]; then
+if [ "$#" -ne 6 ]; then
     echo "Usage: $0 <master_ip> <replica_ip> <port> <gomaxprocs> <thrifty> <epaxos_enabled> <mencius_enabled>"
     exit 1
 fi
 
 master_ip=$1
-replica_ip=$2
-port=$3
-gomaxprocs=$4
-thrifty=$5
-epaxos_enabled=$6
-mencius_enabled=$7
+port=$2
+gomaxprocs=$3
+thrifty=$4
+epaxos_enabled=$5
+mencius_enabled=$6
 
-../bin/server -port $port -maddr $master_ip -addr $replica_ip -p $gomaxprocs -thrifty=$thrifty -e=$epaxos_enabled -m=$mencius_enabled &
+../bin/server -port $port -maddr $master_ip -addr "127.0.0.1" -p $gomaxprocs -thrifty=$thrifty -e=$epaxos_enabled -m=$mencius_enabled &
