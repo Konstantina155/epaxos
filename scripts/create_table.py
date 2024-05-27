3c9fb6aa1f0c70feceb8396525fa8e0ed604b9ed
Konstantina Papafragkaki <105597925+Konstantina155@users.noreply.github.com>
2024-05-27 19:21:04 +0300

    Update create_table.py with the results of the test

--- scripts/create_table.py
+++ scripts/create_table.py
@@ -6,10 +6,10 @@ metrics = ["Throughput", "Median Latency (ms)", "99th Percentile Latency (ms)"]
 df = pd.DataFrame(index=metrics, columns=systems)
 
 values = {
-    "EPaxos (NP)": [...],
-    "Multi-Paxos (NP)": [...],
-    "EPaxos": [...],
-    "Multi-Paxos": [...]
+    "EPaxos (NP)": [766.01, 1.18, 3.23],
+    "Multi-Paxos (NP)": [855.46, 1.14, 1.92],
+    "EPaxos": [1829.13, 0.95, 3.0],
+    "Multi-Paxos": [2289.13, 0.8, 2.49]
 }
 
 for system in systems:
